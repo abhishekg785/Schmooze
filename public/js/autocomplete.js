dd118fc76fbdb04a1b43becba382eb92b43420cb
abhishekg785 <abhishekg785@gmail.com>
2016-11-26 10:16:39 +0530

    autocomplete.js added

--- public/js/autocomplete.js
+++ public/js/autocomplete.js
@@ -9,25 +9,43 @@
   var messageText = $('#messageText');
 
   AutocompleteFunctions = {
+
+    /**
+    *  @param ( String, String )
+    */
     FetchSuggestions : function(lastWord, secondLastWord) {
       console.log('LastWord : ' + lastWord);
       console.log('secondLastWord : ' + secondLastWord); 
-      // $.ajax({
-      //   url : '',
-      //   type : 'POST',
-      //   success : function(data) {
-      //     console.log(data);
-      //   },
-      //   error : function(error) {
-      //     console.log(error);
-      //   }
-      // });
+      var url;
+      if(secondLastWord != undefined) {
+        url = "http://localhost:8080/" + secondLastWord + "/" + lastWord ;
+      }
+      else {
+        url = "http://localhost:8080/" + lastWord + "/";
+      }
+
+      AutocompleteFunctions.SendRequestToFecthData(url);
+    },
+
+    SendRequestToFecthData : function(url) {
+      console.log(url);
+      $.ajax({
+        url : url,
+        type : 'GET', 
+        success : function(data) {
+          console.log(data);
+        },
+        error : function(error) {
+          console.log(error);
+        }
+      });
     }
   }
 
   /*
   * fetch the input string (inputText)
   * find the last word from the inputText and then fetch the next suggestions
+  * if the secondLastWord is undefined , it means user has typed one word
   */
   messageText.on('keyup', function(e) {
     var inputText = messageText.val().trim();
